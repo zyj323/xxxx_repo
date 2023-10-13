26fca080768580a696a7146d1d2d71e39016fe69
zyj <448103153@qq.com>
2023-10-13 14:44:57 +0800

    基础页面

---
+++ app/demo/app/src/main/java/com/example/demo/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.demo.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
